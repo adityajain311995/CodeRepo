6064498d47c5ce5d7a837a939398e46748ca1465
Aditya Jain <ajain219@sapient.com>
2018-07-24 16:31:46 +0530

    no message

--- self-code-programs/src/com/code/programs/MxN_Matrix_Robo_Probability.java
+++ self-code-programs/src/com/code/programs/MxN_Matrix_Robo_Probability.java
@@ -1,11 +1,10 @@
-package com.code.programs.one;
+package com.code.programs;
 /**
  * There's a matrix MxN. A Robo sit on one of the coordinates (m,n).
  * Each 1 second, Robo moves 1 coordinate in one of the 4 directions.(It doesn't stay at same place)
  * If the Robo moves out of the MxN matrix it dies.
  * Find the probability that the Robo is still alive after t seconds.
  * 
- * @author adijain4
  *
  */
 
